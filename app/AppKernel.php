7fb3b34e07c652194f6f2f5323ba818701d256ac
Martin Stangl <martin.stangl@theblacklion.net>
2015-01-05 19:04:08 +0100

    Installed bower for asset handling: Bootstrap, jQuery and AngularJS. (Removed bundles for Bootstrap and jQuery) Added bundle dunglas/angular-csrf-bundle.

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,8 +16,8 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+            new Sp\BowerBundle\SpBowerBundle(),
             new AppBundle\AppBundle(),
-            new Braincrafted\Bundle\BootstrapBundle\BraincraftedBootstrapBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
