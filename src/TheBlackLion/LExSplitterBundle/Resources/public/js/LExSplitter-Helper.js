c2c870c796cf350d1e964677653cff9641e8f8fd
Martin Stangl <martin.stangl@theblacklion.net>
2015-01-05 19:41:31 +0100

    Still trying to fix commit issue with lexsplitter_Helper.js and meanhile also lexsplitter.js :#

---
+++ src/TheBlackLion/LExSplitterBundle/Resources/public/js/LExSplitter-Helper.js
@@ -0,0 +1,3 @@
+/**
+ * Created by Martin on 04.01.2015.
+ */
--- src/TheBlackLion/LExSplitterBundle/Resources/public/js/lexsplitter.js
+++ src/TheBlackLion/LExSplitterBundle/Resources/public/js/lexsplitter.js
@@ -1,7 +1,18 @@
 /**
  * Created by Martin on 02.01.2015.
+ *
+ * 
  */
 
+// Animated scroll to object with tag ID targetID
+function scrollTo (targetID) {
+    $('html, body').animate({scrollTop: $(targetID).offset().top}, 'slow');
+}
+
+// round to n decimal places
+function round (number, places) {
+    return +(Math.round(number + "e+" + places)  + "e-" + places);
+}
 
 
 
